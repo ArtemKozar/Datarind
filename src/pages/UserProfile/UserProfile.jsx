cb074a18c33066f42cdca12c25c3570f842b1a56
Artem Kozar <74992694+ArtemKozar@users.noreply.github.com>
2024-02-07 15:40:07 +0200

    Add navigation for user component (#1)

--- src/pages/UserProfile/UserProfile.jsx
+++ src/pages/UserProfile/UserProfile.jsx
@@ -1,9 +1,29 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import {useParams} from "react-router-dom";
 
 const UserProfile = () => {
+
+    const {id} = useParams()
+    const [userData, setUserData] = useState()
+
+    useEffect(() => {
+        try {
+            fetch(`https://jsonplaceholder.typicode.com/users/${id}`)
+                .then((response) => response.json())
+                .then((json) => setUserData(json));
+
+        } catch (error) {
+            console.log(error)
+        }
+    },[])
+
     return (
         <div>
-            
+            <span>User info</span>
+            <br/>
+            <span>{userData?.name}</span>
+            <br/>
+            <span>{id}</span>
         </div>
     );
 };
