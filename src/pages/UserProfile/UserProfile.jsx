905038ee03d610cc63f17d176c23b675bc57e65f
ArtemKozar <kozarart@gmail.com>
2024-03-26 17:30:55 +0200

    Initialize redux, add eslint config

--- src/pages/UserProfile/UserProfile.jsx
+++ src/pages/UserProfile/UserProfile.jsx
@@ -1,31 +1,29 @@
-import React, {useEffect, useState} from 'react';
-import {useParams} from "react-router-dom";
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
 
 const UserProfile = () => {
+  const { id } = useParams()
+  const [userData, setUserData] = useState()
 
-    const {id} = useParams()
-    const [userData, setUserData] = useState()
+  useEffect(() => {
+    try {
+      fetch(`https://jsonplaceholder.typicode.com/users/${id}`)
+        .then((response) => response.json())
+        .then((json) => setUserData(json))
+    } catch (error) {
+      console.log(error)
+    }
+  }, [])
 
-    useEffect(() => {
-        try {
-            fetch(`https://jsonplaceholder.typicode.com/users/${id}`)
-                .then((response) => response.json())
-                .then((json) => setUserData(json));
+  return (
+    <div>
+      <span>User info</span>
+      <br />
+      <span>{userData?.name}</span>
+      <br />
+      <span>{id}</span>
+    </div>
+  )
+}
 
-        } catch (error) {
-            console.log(error)
-        }
-    },[])
-
-    return (
-        <div>
-            <span>User info</span>
-            <br/>
-            <span>{userData?.name}</span>
-            <br/>
-            <span>{id}</span>
-        </div>
-    );
-};
-
-export default UserProfile;
+export default UserProfile
--- src/pages/UsersList/UsersList.jsx
+++ src/pages/UsersList/UsersList.jsx
@@ -1,39 +1,38 @@
-import React, {useEffect, useState} from 'react'
-import UserItem from "./components/UserItem/UserItem";
-import {Link} from "react-router-dom";
+import { useEffect, useState } from 'react'
+
+import UserItem from './components/UserItem/UserItem'
+import { Link } from 'react-router-dom'
 
 const UsersList = () => {
+  const [dataUsers, setDataUsers] = useState()
 
-    const [dataUsers, setDataUsers] = useState()
-
-    useEffect(() => {
-        try {
-            fetch('https://jsonplaceholder.typicode.com/users')
-                .then((response) => response.json())
-                .then((json) => setDataUsers(json));
-        } catch (error) {
-            console.log(error)
-        }
-
-    },[])
+  useEffect(() => {
+    try {
+      fetch('https://jsonplaceholder.typicode.com/users')
+        .then((response) => response.json())
+        .then((json) => setDataUsers(json))
+    } catch (error) {
+      console.log(error)
+    }
+  }, [])
 
   return (
     <>
       <div>UsersList</div>
-        {dataUsers?.map((user) => {
-            return (
-                <Link to={`/${user.id}`}>
-                    <UserItem
-                        key={user.id}
-                        name={user.name}
-                        phone={user.phone}
-                        email={user.email}
-                        id={user.id}
-                        address={user.address}
-                    />
-                </Link>
-            )
-        })}
+      {dataUsers?.map((user) => {
+        return (
+          <Link to={`/${user.id}`}>
+            <UserItem
+              key={user.id}
+              name={user.name}
+              phone={user.phone}
+              email={user.email}
+              id={user.id}
+              address={user.address}
+            />
+          </Link>
+        )
+      })}
     </>
   )
 }
