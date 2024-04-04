cde26b0c06cf0d23fd8867bfd7c987a968f1ef25
Artem Kozar <akozar@softserveinc.com>
2024-04-04 21:30:57 +0300

    Change user data

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,44 +1,16 @@
-import React from 'react'
-import Parse from 'parse/dist/parse.min'
+import React from "react";
 
-import styles from './Home.module.scss'
+import styles from "./Home.module.scss";
 
 const Home = () => {
-  Parse.serverURL = 'https://parseapi.back4app.com'
-  Parse.initialize(
-    'O0jzbt32Ax63tVUuBNF4MhiqIOsXHajyVYbfc18I',
-    'YFgtM3aq5g4mLZhs4iyVlJ6Oka73Cts5aQtubgCi',
-    'wUIvpjuvaSHQBFqhY8VM3U6QEIp11mnRpxj4fuHc'
-  )
-  ;(async () => {
-    const Employees = Parse.Object.extend('Employees')
-    const query = new Parse.Query(Employees)
-    try {
-      const results = await query.find()
-      for (const object of results) {
-        // Access the Parse Object attributes using the .GET method
-        const Name = object.get('Name')
-        const LastName = object.get('LastName')
-
-        console.log(Name)
-        console.log(LastName)
-        console.log(object.attributes.Name, '- name')
-      }
-    } catch (error) {
-      console.error('Error while fetching Employees', error)
-    }
-  })()
-
   return (
     <>
-   <div>Home
-   </div>
-
+      <div>Home</div>
       <div>Home </div>
       <div>Home </div>
       <div>Home </div>
     </>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
--- src/pages/UsersList/components/UserItem/UserItem.jsx
+++ src/pages/UsersList/components/UserItem/UserItem.jsx
@@ -1,34 +1,33 @@
-import React from 'react';
+import React from "react";
 import PropTypes from "prop-types";
 
-import styles from './UserItem.module.scss'
+import styles from "./UserItem.module.scss";
 
-const UserItem = ({name, phone, email}) => {
-
-    return (
-        <div className={styles.userBlock}>
-            <div>{name}</div>
-            <div>
-                <div>{phone}</div>
-                <div>{email}</div>
-            </div>
-            <div>Working hours - {Math.round(Math.random()*100)}</div>
-            <div>{new Date().toDateString()}</div>
-        </div>
-    );
-}
+const UserItem = ({ name, phone, email, id }) => {
+  return (
+    <div className={styles.userBlock}>
+      <div>{name}</div>
+      <div>
+        <div>{phone}</div>
+        <div>{email}</div>
+      </div>
+      <div>Working hours - {Math.round(Math.random() * 100)}</div>
+      <div>{new Date().toDateString()}</div>
+    </div>
+  );
+};
 
 UserItem.propTypes = {
-    name: PropTypes.string.isRequired,
-    phone: PropTypes.string.isRequired,
-    email: PropTypes.string.isRequired,
-    id: PropTypes.number,
-    address: PropTypes.object
-}
+  name: PropTypes.string.isRequired,
+  phone: PropTypes.number.isRequired,
+  email: PropTypes.string.isRequired,
+  id: PropTypes.string,
+  address: PropTypes.object,
+};
 
 UserItem.defaultProps = {
-    id: 0,
-    address: {}
-}
+  id: "",
+  address: {},
+};
 
 export default UserItem;
