bd3ad1e353d8af1c3fd43b8fcdf86c86b26b3ed9
Artem Kozar <74992694+ArtemKozar@users.noreply.github.com>
2024-04-04 16:31:39 +0300

    Configured redux for new user, add module for id (#7)
    Co-authored-by: Artem Kozar <akozar@softserveinc.com>

--- src/pages/AddUser/AddUser.jsx
+++ src/pages/AddUser/AddUser.jsx
@@ -1,40 +1,79 @@
-import React from 'react'
-import DatePicker from 'react-datepicker'
-import 'react-datepicker/dist/react-datepicker.css'
-import { useState } from 'react'
+import React from "react";
+import DatePicker from "react-datepicker";
+import { v4 as uuidv4 } from "uuid";
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { addEmployee } from "../../redux/employees/actionCreators";
 
-import styles from './AddUser.module.scss'
+import "react-datepicker/dist/react-datepicker.css";
+import styles from "./AddUser.module.scss";
 
 const AddUser = () => {
-  const [selectedDate, setSelectedDate] = useState(null)
-  console.log(selectedDate)
+  const [name, setName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [position, setPosition] = useState("");
+  const [birthday, setBirthday] = useState(null);
+  const dispatch = useDispatch();
+
+  const createNewuser = () => {
+    const newUser = {
+      id: uuidv4(),
+      name: name,
+      lastName: lastName,
+      email: email,
+      position: position,
+    };
+    dispatch(addEmployee(newUser));
+  };
+
   return (
     <div>
-      <form action="">
-        <label>
-          Name:
-          <input type="text" />
-        </label>
-        <label>
-          Last Name:
-          <input type="text" />
-        </label>
-        <label>
-          Email:
-          <input type="text" />
-        </label>
-        <label>
-          Birthday:
-          <DatePicker
-            selected={selectedDate}
-            onChange={(date) => setSelectedDate(date)}
-            dateFormat="dd/MM/yyyy" // Формат дати, який ви хочете використовувати
-          />
-        </label>
-        <button type="submit">Save</button>
-      </form>
+      <label>
+        Name:
+        <input
+          type="text"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+      </label>
+      <label>
+        Last Name:
+        <input
+          type="text"
+          value={lastName}
+          onChange={(e) => setLastName(e.target.value)}
+        />
+      </label>
+      <label>
+        Email:
+        <input
+          type="text"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+      </label>
+      <label>
+        Position:
+        <input
+          type="text"
+          value={position}
+          onChange={(e) => setPosition(e.target.value)}
+        />
+      </label>
+      <label>
+        Birthday:
+        <DatePicker
+          selected={birthday}
+          onChange={(date) => setBirthday(date)}
+          dateFormat="dd/MM/yyyy"
+        />
+      </label>
+      <button type="submit" onClick={createNewuser}>
+        Save
+      </button>
     </div>
-  )
-}
+  );
+};
 
-export default AddUser
+export default AddUser;
